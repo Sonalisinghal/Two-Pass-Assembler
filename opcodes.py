a5f473fe9167ada85da8302ef010eda1238f672a
Bhavya <bhavya18333@iiitd.ac.in>
2019-09-27 01:19:11 +0530

    added number of instructions per opcode

--- opcodes.py
+++ opcodes.py
@@ -1,1 +1,2 @@
 opcodes={'CLA':'0000','LAC':'0001','SAC':'0010','ADD':'0011','SUB':'0100','BRZ':'0101','BRN':'0110','BRP':'0111','INP':'1000','DSP':'1001','MUL':'1010','DIV':'1011','STP':'1100'}
+opcode_arguments={'CLA':0,'LAC':1,'SAC':1,'ADD':1,'SUB':1,'BRZ':1,'BRN':1,'BRP':1,'INP':1,'DSP':1,'MUL':1,'DIV':3,'STP':0}
