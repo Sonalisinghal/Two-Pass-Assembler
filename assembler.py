64653e9189138287f44bd118846ba9b839d3c3da
Bhavya <bhavya18333@iiitd.ac.in>
2019-10-02 15:21:40 +0530

    handled comments and added labels to symbol table

--- assembler.py
+++ assembler.py
@@ -1,31 +1,49 @@
 from opcodes import opcodes
 
-labelTable = {}
 symbolTable = {}
 literalTable = {}
 opcodeTable = {}
+instructions = []
+num_ins = 0
 
-def containsLabel(line):
-	if(line.find(":")!=-1):
+bin8 = lambda x : ''.join(reversed([str((x >> i) & 1) for i in range(8)] ) )
+
+def containsLabel(token):
+	if(token.find(":")!=-1):
 		return(True)
 	else:
 		return(False)
 
-path = input("Enter file path: ")
-path = "./Sample_Inputs/"+path
-f = open(path,'r') #first pass
-line = f.readline()
+def addLabel(label, address):
+	symbolTable[label]=address	
 
-while line:
-	'''
-	instruction=list(line.split())
-	if len(instruction)<2:
-		instruction.insert(0,'')
-		print(instruction)
-	'''
-	print(line)
-	line=f.readline()
-	if(containsLabel(line)):
-		print(list(line.split()))
-	else:
-		print(list(line.split()))
+def containsVariable(instruction):
+	pass
+
+def containsLiteral(instruction):
+	pass
+
+path = input("Enter file path: ")
+path = "./Sample_Inputs/"+path+".txt"
+f = open(path,'r')
+
+instruction = f.readline()
+while instruction:
+	if(instruction.find(";")!=(-1)):
+		instruction = instruction[0:instruction.find(";")]
+	instruction = list(instruction.split())
+	instruction = [bin8(num_ins)]+instruction
+	instructions.append(instruction)
+	
+	if(containsLabel(instruction[1])):
+		addLabel(instruction[1][0:-1], instruction[0])
+	if(containsVariable(instruction)):
+		pass
+	if(containsLiteral(instruction)):
+		pass
+
+	num_ins+=1
+	instruction=f.readline()
+
+print(instructions)
+print(symbolTable)
